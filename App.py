0539304ef2f721966e4f4b0c55552b9b4dd89072
Andres_enriquez <andres_alb.enriquez@uao.edu.co>
2023-08-15 18:51:37 -0500

    Aplicacion funcional

---
+++ App.py
@@ -0,0 +1,30 @@
+ #Importar librerias necesarias
+
+import sqlalchemy 
+import pandas as pd
+from sqlalchemy import create_engine 
+
+ruta_archivo = input ("Porfavor ingrese el nombre del archivo:")
+       
+try: 
+    archivo =pd.read_csv(f"{ruta_archivo}", sep=';')  
+    
+
+except FileNotFoundError:
+    print ("El archivo no ha sido encontrado")
+ 
+
+else:
+    print ("Su arvhivo ha sido ingresado") 
+
+nombre_tabla = input ("Ingrese un nombre para la tabla:") 
+
+engine = create_engine('postgresql://postgres:Basesdedatosandres@localhost:5432/postgres') 
+    
+archivo.to_sql(f"{nombre_tabla}"  , engine)    
+
+print ("Su tabla ha sido subida a postgresSQL") 
+
+
+
+
