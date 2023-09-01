0b8262d427b5ceebce4b607ce1bf1a57ad0478bc
Andres_enriquez <andres_alb.enriquez@uao.edu.co>
2023-09-01 16:05:34 -0500

    Modificacion codigo funcional

--- App.py
+++ App.py
@@ -1,10 +1,10 @@
- #Importar librerias necesarias
+#Importar librerias necesarias
 
 import sqlalchemy 
 import pandas as pd
 from sqlalchemy import create_engine 
 
-ruta_archivo = input ("Porfavor ingrese el nombre del archivo:")
+ruta_archivo = input ("Porfavor ingrese la ruta y el nombre del archivo:")
        
 try: 
     archivo =pd.read_csv(f"{ruta_archivo}", sep=';')  
@@ -19,11 +19,12 @@ else:
 
 nombre_tabla = input ("Ingrese un nombre para la tabla:") 
 
-engine = create_engine('postgresql://postgres:Basesdedatosandres@localhost:5432/postgres') 
+engine = create_engine('postgresql://postgres:Basesdedatosandres@localhost:5432/votaciones') 
     
 archivo.to_sql(f"{nombre_tabla}"  , engine)    
 
 print ("Su tabla ha sido subida a postgresSQL") 
+
 
 
 
